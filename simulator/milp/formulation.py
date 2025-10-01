09ee8bbaca703002088f14c2e44d5ca75491c1ad
CeciliaAlmeida <ceciliadesousaluzalmeida@gmail.com>
2025-10-01 11:24:10 +0200

    "Milp solve implemented"

--- simulator/milp/formulation.py
+++ simulator/milp/formulation.py
@@ -1,15 +1,139 @@
+from collections import defaultdict
+
 class MILPInstance:
-    """
-    Defines sets and parameters of the MILP instance.
-    """
-    def __init__(self, N, E, S, V_of_s, CPU_i, CPUcap, BW_s, BWcap, Lat, Ls):
-        self.N = N                  # list of nodes
-        self.E = E                  # list of edges
-        self.S = S                  # list of slices
-        self.V_of_s = V_of_s        # dict: slice -> list of VNFs
-        self.CPU_i = CPU_i          # dict: vnf -> cpu demand
-        self.CPUcap = CPUcap        # dict: node -> cpu capacity
-        self.BW_s = BW_s            # dict: slice -> bandwidth demand
-        self.BWcap = BWcap          # dict: edge -> bandwidth capacity
-        self.Lat = Lat              # dict: edge -> latency
-        self.Ls = Ls                # dict: slice -> latency budget
+    def __init__(self, G, slices):
+        self.N = list(G.nodes())
+        self.E = list(G.edges())  
+        self.S = list(range(len(slices)))
+        self.V_of_s = {s: [vnf["id"] for vnf in slices[s][0]] for s in self.S}
+        self.CPU_i = {vnf["id"]: vnf["cpu"] for s in slices for vnf in s[0]}
+        self.BW_s = {s: min(vnf["throughput"] for vnf in slices[s][0]) for s in self.S}
+        self.L_s = {s: max(vnf["latency"] for vnf in slices[s][0]) for s in self.S}
+
+        self.CPU_cap = {n: G.nodes[n]["cpu"] for n in self.N}
+        self.BW_cap = {e: G.edges[e]["bandwidth"] for e in self.E}
+        self.lat_e = {e: G.edges[e]["latency"] for e in self.E}
+
+    def build_lp(self):
+        c = []        
+        A_eq, b_eq = [], []
+        A_ub, b_ub = [], []
+        bounds = []
+        var_index = {}
+
+        idx = 0
+
+    
+        for i in self.CPU_i:
+            for n in self.N:
+                var_index[("v", i, n)] = idx
+                c.append(0)   
+                bounds.append((0, 1))
+                idx += 1
+
+ 
+        for n in self.N:
+            var_index[("u", n)] = idx
+            c.append(1)   
+            bounds.append((0, 1))
+            idx += 1
+
+            var_index[("z", n)] = idx
+            c.append(1)  
+            bounds.append((0, 1))
+            idx += 1
+
+       
+        for s in self.S:
+            vnf_ids = self.V_of_s[s]
+            for q in range(len(vnf_ids) - 1):
+                i, j = vnf_ids[q], vnf_ids[q + 1]
+                for e in self.E:
+                    var_index[("f", e, s, (i, j))] = idx
+                    c.append(0)  
+                    bounds.append((0, 1))
+                    idx += 1
+
+        for e in self.E:
+            var_index[("rho", e)] = idx
+            c.append(1)   
+            bounds.append((0, 1))
+            idx += 1
+
+            var_index[("w", e)] = idx
+            c.append(1)  
+            bounds.append((0, 1))
+            idx += 1
+
+      
+        for n in self.N:
+            row = [0] * idx
+            for i in self.CPU_i:
+                row[var_index[("v", i, n)]] = self.CPU_i[i]
+            row[var_index[("u", n)]] = -self.CPU_cap[n]
+            A_ub.append(row)
+            b_ub.append(0)
+
+            # u_n <= z_n
+            row = [0] * idx
+            row[var_index[("u", n)]] = 1
+            row[var_index[("z", n)]] = -1
+            A_ub.append(row)
+            b_ub.append(0)
+
+        for i in self.CPU_i:
+            row = [0] * idx
+            for n in self.N:
+                row[var_index[("v", i, n)]] = 1
+            A_eq.append(row)
+            b_eq.append(1)
+
+     
+        for s in self.S:
+            vnf_ids = self.V_of_s[s]
+            for q in range(len(vnf_ids) - 1):
+                i, j = vnf_ids[q], vnf_ids[q + 1]
+                for n in self.N:
+                    row = [0] * idx
+                    for e in self.E:
+                        u, v = e
+                        if u == n:
+                            row[var_index[("f", e, s, (i, j))]] += 1
+                        if v == n:
+                            row[var_index[("f", e, s, (i, j))]] -= 1
+                    row[var_index[("v", i, n)]] += 1
+                    row[var_index[("v", j, n)]] -= 1
+                    A_eq.append(row)
+                    b_eq.append(0)
+
+    
+        for e in self.E:
+            row = [0] * idx
+            for s in self.S:
+                vnf_ids = self.V_of_s[s]
+                for q in range(len(vnf_ids) - 1):
+                    i, j = vnf_ids[q], vnf_ids[q + 1]
+                    row[var_index[("f", e, s, (i, j))]] = self.BW_s[s]
+            row[var_index[("rho", e)]] = -self.BW_cap[e]
+            A_ub.append(row)
+            b_ub.append(0)
+
+            # rho_e <= w_e
+            row = [0] * idx
+            row[var_index[("rho", e)]] = 1
+            row[var_index[("w", e)]] = -1
+            A_ub.append(row)
+            b_ub.append(0)
+
+        
+        for s in self.S:
+            vnf_ids = self.V_of_s[s]
+            for q in range(len(vnf_ids) - 1):
+                i, j = vnf_ids[q], vnf_ids[q + 1]
+                row = [0] * idx
+                for e in self.E:
+                    row[var_index[("f", e, s, (i, j))]] = self.lat_e[e]
+                A_ub.append(row)
+                b_ub.append(self.L_s[s])
+
+        return c, A_ub, b_ub, A_eq, b_eq, bounds, var_index
--- simulator/milp/solver_scipy.py
+++ simulator/milp/solver_scipy.py
@@ -2,40 +2,60 @@
 # All comments in English
 
 import numpy as np
-from scipy.optimize import linprog
+from scipy.optimize import milp, LinearConstraint, Bounds
 
 def build_variable_index(instance):
     """
     Builds a dictionary mapping each decision variable to a position in the x vector.
+    Also prepares integrality information for MILP.
     """
     var_index = {}
     idx = 0
-    # v_{i,n}
+    integrality = []
+
+    # v_{i,n} ∈ {0,1}
     for s in instance.S:
         for i in instance.V_of_s[s]:
             for n in instance.N:
-                var_index[("v", i, n)] = idx; idx += 1
-    # l_{e}^{(s)}
+                var_index[("v", i, n)] = idx
+                integrality.append(2)  # binary
+                idx += 1
+
+    # l_{e}^{(s)} ∈ {0,1}
     for s in instance.S:
         for e in instance.E:
-            var_index[("l", e, s)] = idx; idx += 1
-    # u_n, z_n
-    for n in instance.N:
-        var_index[("u", n)] = idx; idx += 1
-        var_index[("z", n)] = idx; idx += 1
-    # rho_e, w_e
-    for e in instance.E:
-        var_index[("rho", e)] = idx; idx += 1
-        var_index[("w", e)] = idx; idx += 1
+            var_index[("l", e, s)] = idx
+            integrality.append(2)  # binary
+            idx += 1
 
-    return var_index, idx
+    # u_n ∈ [0,1] continuous, z_n ∈ {0,1}
+    for n in instance.N:
+        var_index[("u", n)] = idx
+        integrality.append(0)  # continuous
+        idx += 1
+
+        var_index[("z", n)] = idx
+        integrality.append(2)  # binary
+        idx += 1
+
+    # rho_e ∈ [0,1] continuous, w_e ∈ {0,1}
+    for e in instance.E:
+        var_index[("rho", e)] = idx
+        integrality.append(0)  # continuous
+        idx += 1
+
+        var_index[("w", e)] = idx
+        integrality.append(2)  # binary
+        idx += 1
+
+    return var_index, idx, np.array(integrality)
 
 
 def solve_lp(instance):
     """
-    Solves the LP relaxation of the MILP with scipy.optimize.linprog
+    Solves the MILP using scipy.optimize.milp (HiGHS backend).
     """
-    var_index, n_vars = build_variable_index(instance)
+    var_index, n_vars, integrality = build_variable_index(instance)
 
     # Objective vector c
     c = np.zeros(n_vars)
@@ -47,7 +67,8 @@ def solve_lp(instance):
         c[var_index[("w", e)]] = 1
 
     # Constraints
-    A_ub, b_ub, A_eq, b_eq = [], [], [], []
+    A_ub, b_ub = [], []
+    A_eq, b_eq = [], []
 
     # -------------------------------
     # Node CPU capacity
@@ -58,14 +79,14 @@ def solve_lp(instance):
             for i in instance.V_of_s[s]:
                 row[var_index[("v", i, n)]] = instance.CPU_i[i]
         A_ub.append(row)
-        b_ub.append(instance.CPUcap[n])
+        b_ub.append(instance.CPU_cap[n])
 
     # Definition of u_n
     for n in instance.N:
         row = np.zeros(n_vars)
         for s in instance.S:
             for i in instance.V_of_s[s]:
-                row[var_index[("v", i, n)]] = instance.CPU_i[i] / instance.CPUcap[n]
+                row[var_index[("v", i, n)]] = instance.CPU_i[i] / instance.CPU_cap[n]
         row[var_index[("u", n)]] = -1
         A_eq.append(row)
         b_eq.append(0.0)
@@ -86,13 +107,13 @@ def solve_lp(instance):
         for s in instance.S:
             row[var_index[("l", e, s)]] = instance.BW_s[s]
         A_ub.append(row)
-        b_ub.append(instance.BWcap[e])
+        b_ub.append(instance.BW_cap[e])
 
     # Definition of rho_e
     for e in instance.E:
         row = np.zeros(n_vars)
         for s in instance.S:
-            row[var_index[("l", e, s)]] = instance.BW_s[s] / instance.BWcap[e]
+            row[var_index[("l", e, s)]] = instance.BW_s[s] / instance.BW_cap[e]
         row[var_index[("rho", e)]] = -1
         A_eq.append(row)
         b_eq.append(0.0)
@@ -133,20 +154,24 @@ def solve_lp(instance):
     for s in instance.S:
         row = np.zeros(n_vars)
         for e in instance.E:
-            row[var_index[("l", e, s)]] = instance.Lat[e]
+            row[var_index[("l", e, s)]] = instance.lat_e[e]
         A_ub.append(row)
-        b_ub.append(instance.Ls[s])
+        b_ub.append(instance.L_s[s])
 
     # -------------------------------
-    # Solve LP relaxation
+    # Solve MILP
     # -------------------------------
-    A_ub = np.array(A_ub) if A_ub else None
-    b_ub = np.array(b_ub) if b_ub else None
-    A_eq = np.array(A_eq) if A_eq else None
-    b_eq = np.array(b_eq) if b_eq else None
+    constraints = []
+    if A_ub:
+        constraints.append(LinearConstraint(np.array(A_ub), -np.inf, np.array(b_ub)))
+    if A_eq:
+        constraints.append(LinearConstraint(np.array(A_eq), np.array(b_eq), np.array(b_eq)))
 
-    res = linprog(c, A_ub=A_ub, b_ub=b_ub,
-                  A_eq=A_eq, b_eq=b_eq,
-                  bounds=(0, 1), method="highs")
+    bounds = Bounds(0, 1)
+
+    res = milp(c=c,
+               constraints=constraints,
+               integrality=integrality,
+               bounds=bounds)
 
     return res, var_index
